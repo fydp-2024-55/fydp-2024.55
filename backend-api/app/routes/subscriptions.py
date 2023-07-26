544258607736ccfe2b8f5d64c65f5971ad01d0ff
William Park <w22park@uwaterloo.ca>
2023-07-26 01:07:11 -0400

    update /subscriptions endpoints

--- backend-api/app/routes/subscriptions.py
+++ backend-api/app/routes/subscriptions.py
@@ -1,11 +1,16 @@
-import random
-
 from datetime import datetime
-from fastapi import APIRouter, status, Request, HTTPException
+from fastapi import APIRouter, status, Request, HTTPException, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from ..dependencies import get_current_active_user, get_async_session
+from ..models.users import User
+from ..models.producers import Producer
 from ..schemas.subscriptions import (
-    SubscriptionCreate,
-    SubscriptionRead,
+    SubscriptionsCreate,
+    SubscriptionsRead,
+    SubscriptionsUpdate,
+    SubscriptionsAvailable,
 )
 from ..utils.date import date_to_epoch
 from ..blockchain.subscription import consumer_purchase_tokens
@@ -15,7 +20,11 @@ router = APIRouter()
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-async def create_subscriptions(subscriptions: SubscriptionCreate, request: Request):
+async def create_subscriptions(
+    subscriptions: SubscriptionsCreate,
+    request: Request,
+    consumer: User = Depends(get_current_active_user),
+):
     # Check for a valid expiration date
     current_date = datetime.now().date()
     expiration_date = subscriptions.expiration_date
@@ -27,64 +36,79 @@ async def create_subscriptions(subscriptions: SubscriptionCreate, request: Reque
     # Perform the token purchases through the smart contract
     consumer_purchase_tokens(
         request.app.state.token_contract,
-        subscriptions.consumer_eth_address,
+        consumer.eth_address,
         subscriptions.producer_eth_addresses,
         date_to_epoch(current_date),
         date_to_epoch(expiration_date),
     )
 
     subscriptions = consumer_subscriptions(
-        request.app.state.token_contract, subscriptions.consumer_eth_address
+        request.app.state.token_contract, consumer.eth_address
     )
 
-    return SubscriptionRead(subscriptions=subscriptions)
+    return SubscriptionsRead(subscriptions=subscriptions)
 
 
-@router.get("/{consumer_eth_address}", status_code=status.HTTP_200_OK)
-async def read_subscriptions(consumer_eth_address: str, request: Request):
+@router.get("/me", status_code=status.HTTP_200_OK)
+async def read_subscriptions(
+    request: Request,
+    consumer: User = Depends(get_current_active_user),
+):
     subscriptions = consumer_subscriptions(
-        request.app.state.token_contract, consumer_eth_address
+        request.app.state.token_contract, consumer.eth_address
     )
 
-    return SubscriptionRead(subscriptions=subscriptions)
+    return SubscriptionsRead(subscriptions=subscriptions)
 
 
-@router.patch("/{consumer_eth_address}", status_code=status.HTTP_200_OK)
-async def update_subscriptions(consumer_eth_address: str, request: Request):
+@router.patch("/me", status_code=status.HTTP_200_OK)
+async def update_subscriptions(
+    request: Request, consumer: User = Depends(get_current_active_user)
+):
     # TODO: Implement later
 
     subscriptions = consumer_subscriptions(
-        request.app.state.token_contract, consumer_eth_address
+        request.app.state.token_contract, consumer.eth_address
     )
 
-    return SubscriptionRead(subscriptions=subscriptions)
+    return SubscriptionsRead(subscriptions=subscriptions)
 
 
-@router.delete("/{consumer_eth_address}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_subscriptions(consumer_eth_address: str):
+@router.delete("/me", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_subscriptions():
     # TODO: Implement later
 
     return
 
 
 @router.get("/available")
-async def read_subscriptions_available():
-    return [str(random.randint(1, 1000)) for _ in range(5)]
+async def read_subscriptions_available(
+    min_age: int | None = None,
+    max_age: int | None = None,
+    gender: list[str] | None = None,
+    ethnicity: list[str] | None = None,
+    country: list[str] | None = None,
+    min_income: int | None = None,
+    max_income: int | None = None,
+    marital_status: list[str] | None = None,
+    parental_status: list[str] | None = None,
+    session: AsyncSession = Depends(get_async_session),
+):
+    query = select(Producer.eth_address)
+    query = query.where(Producer.age >= min_age) if min_age else query
+    query = query.where(Producer.age <= max_age) if min_age else query
+    query = query.where(Producer.gender.in_(gender)) if min_age else query
+    query = query.where(Producer.ethnicity.in_(ethnicity)) if min_age else query
+    query = query.where(Producer.country.in_(country)) if min_age else query
+    query = query.where(Producer.income >= min_income) if min_age else query
+    query = query.where(Producer.income <= max_income) if min_age else query
+    query = (
+        query.where(Producer.marital_status.in_(marital_status)) if min_age else query
+    )
+    query = (
+        query.where(Producer.parental_status.in_(parental_status)) if min_age else query
+    )
 
+    eth_addresses = await session.execute(query)
 
-# user_data_dict = {
-#     "gender": "M",
-#     "ethnicity": "B",
-#     "date_of_birth": "2023-07-20",
-#     "city": "Santa Clara",
-#     "state": "CA",
-#     "country": "US",
-#     "income": 450000,
-#     "marital_status": "not single anymore",
-#     "parental_status": "parents and step parents",
-# }
-
-
-# @router.get("/data")
-# async def read_subscriptions_users():
-#     return [UserData(id=random.randint(1, 1000), **user_data_dict) for _ in range(5)]
+    return SubscriptionsAvailable(eth_addresses=eth_addresses)
--- backend-api/app/schemas/subscriptions.py
+++ backend-api/app/schemas/subscriptions.py
@@ -3,34 +3,28 @@ from datetime import date
 from pydantic import BaseModel
 
 
-class SubsciptionItem(BaseModel):
-    producer_eth_address: str
+class SubscriptionsBase(BaseModel):
+    eth_addresses: list[str]
+
+
+class SubscriptionsCreate(SubscriptionsBase):
+    expiration_date: date
+
+
+class SubscriptionItem(BaseModel):
+    eth_address: str
     creation_date: date
     expiration_date: date
     active: bool
 
 
-class SubscriptionCreate(BaseModel):
-    consumer_eth_address: str
-    producer_eth_addresses: list[str]
-    expiration_date: date
+class SubscriptionsRead(BaseModel):
+    subscriptions: list[SubscriptionItem]
 
 
-class SubscriptionRead(BaseModel):
-    subscriptions: list[SubsciptionItem]
+class SubscriptionsUpdate(SubscriptionsBase):
+    pass
 
 
-class SubscriptionUpdate(BaseModel):
-    producer_eth_addresses: list[str]
-
-
-# class UserData(BaseModel):
-#     gender: str
-#     ethnicity: str
-#     date_of_birth: date
-#     city: str
-#     state: str
-#     country: str
-#     income: int
-#     marital_status: str
-#     parental_status: str
+class SubscriptionsAvailable(SubscriptionsBase):
+    pass
