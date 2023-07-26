5923993cd0e0a59b31597277fdd26e070d4fea65
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2023-07-25 21:54:10 -0700

    Fix many to many revisions
    Co-authored-by: Abhyuday Bose <abhyudaybose@users.noreply.github.com>

--- backend-api/app/models/histories.py
+++ backend-api/app/models/histories.py
@@ -1,17 +1,18 @@
-from ..database import Base
 import sqlalchemy as sa
 from sqlalchemy.orm import relationship
+
+from ..database import Base
+from .producers import Producer
 
 
 class History(Base):
     __tablename__ = "Histories"
+
     id = sa.Column(sa.Integer, primary_key=True, index=True, nullable=False)
     title = sa.Column(sa.String, nullable=False)
     visit_time = sa.Column(sa.DateTime, nullable=False)
     time_spent = sa.Column(sa.Integer, nullable=False)
-    producer_id = sa.Column(sa.Integer, sa.ForeignKey("Producers.id"), nullable=False)
     url = sa.Column(sa.String, nullable=False)
+    producer_id = sa.Column(sa.Integer, sa.ForeignKey("Producers.id"), nullable=False)
 
-    categories = relationship(
-        "Category", secondary="History_Categories", back_populates="histories"
-    )
+    producer = relationship(Producer, back_populates="histories")
--- backend-api/app/models/producers.py
+++ backend-api/app/models/producers.py
@@ -1,16 +1,18 @@
-from ..database import Base
 import sqlalchemy as sa
-from . import users
 from sqlalchemy.orm import relationship
 
+from ..database import Base
+from .locations import Location
+from .users import User
 
-class Producer(users.User, Base):
+
+class Producer(Base):
     __tablename__ = "Producers"
+
     id = sa.Column(sa.Integer, primary_key=True, index=True, nullable=False)
     user_id = sa.Column(
         sa.Integer, sa.ForeignKey("Users.id"), index=True, nullable=False
     )
-    eth_address = sa.Column(sa.String, unique=True, index=True, nullable=False)
     name = sa.Column(sa.String, nullable=False)
     location_id = sa.Column(sa.Integer, sa.ForeignKey("Locations.id"), nullable=False)
     date_of_birth = sa.Column(sa.Date, nullable=False)
@@ -20,9 +22,5 @@ class Producer(users.User, Base):
     marital_status = sa.Column(sa.CHAR, nullable=False)
     parental_status = sa.Column(sa.CHAR, nullable=False)
 
-    restricted_categories = relationship(
-        "Category",
-        secondary="Producer_Restricted_Categories",
-        back_populates="restricting_producers",
-    )
-    location = relationship("Location", back_populates="producers")
+    user = relationship(User, back_populates="producer")
+    location = relationship(Location, back_populates="producers")
