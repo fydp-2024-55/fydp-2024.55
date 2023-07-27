c82a28f189339d2c9a7a8a502fbca2faed797cad
Abhyuday Bose <56177851+abhyudaybose@users.noreply.github.com>
2023-07-27 00:53:53 -0400

    Added crud operations for producers, consumers, and histories (#14)
    Co-authored-by: Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
    Co-authored-by: Abhyuday Bose <abhyudaybose@users.noreply.github.com>

---
+++ backend-api/app/ops/consumers.py
@@ -0,0 +1,37 @@
+import sqlalchemy as sa
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from ..models.consumers import Consumer
+from ..models.users import User
+from ..schemas.consumers import ConsumerCreate, ConsumerRead, ConsumerUpdate
+
+
+async def get_consumer(db: AsyncSession, user: User):
+    statement = sa.select(Consumer).where(Consumer.user_id == user.id)
+    result = await db.execute(statement)
+    consumer = result.scalar_one_or_none()
+    if consumer is None:
+        return None
+    return ConsumerRead(**consumer.__dict__)
+
+
+async def create_consumer(db: AsyncSession, consumer: ConsumerCreate, user: User):
+    statement = sa.insert(Consumer).values(**consumer.model_dump(), user_id=user.id)
+    await db.execute(statement)
+    await db.commit()
+
+
+async def update_consumer(db: AsyncSession, consumer: ConsumerUpdate, user: User):
+    statement = (
+        sa.update(Consumer)
+        .values(**consumer.model_dump())
+        .where(Consumer.user_id == user.id)
+    )
+    await db.execute(statement)
+    await db.commit()
+
+
+async def delete_consumer(db: AsyncSession, user: User):
+    statement = sa.delete(Consumer).where(Consumer.user_id == user.id)
+    await db.execute(statement)
+    await db.commit()
