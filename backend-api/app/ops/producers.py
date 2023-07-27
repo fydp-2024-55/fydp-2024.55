0a7545c5cb4ee932aad6f96cdffe4fd478cb46e9
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2023-07-26 22:49:10 -0700

    Mint and burn producer tokens

--- backend-api/app/ops/producers.py
+++ backend-api/app/ops/producers.py
@@ -12,19 +12,54 @@ async def get_producer(db: AsyncSession, user: User):
     producer = result.scalar_one_or_none()
     if producer is None:
         return None
-    return ProducerRead(**producer.__dict__)
+    return ProducerRead(**producer.__dict__, eth_address=user.eth_address)
 
 
 async def create_producer(db: AsyncSession, producer: ProducerCreate, user: User):
-    statement = sa.insert(Producer).values(**producer.model_dump(), user_id=user.id)
+    if producer.eth_address:
+        statement = (
+            sa.update(User)
+            .values(eth_address=producer.eth_address)
+            .where(User.id == user.id)
+        )
+        await db.execute(statement)
+
+    statement = sa.insert(Producer).values(
+        user_id=user.id,
+        name=producer.name,
+        country=producer.country,
+        date_of_birth=producer.date_of_birth,
+        gender=producer.gender,
+        ethnicity=producer.ethnicity,
+        income=producer.income,
+        marital_status=producer.marital_status,
+        parental_status=producer.parental_status,
+    )
     await db.execute(statement)
     await db.commit()
 
 
 async def update_producer(db: AsyncSession, producer: ProducerUpdate, user: User):
+    if producer.eth_address:
+        statement = (
+            sa.update(User)
+            .values(eth_address=producer.eth_address)
+            .where(User.id == user.id)
+        )
+    await db.execute(statement)
+
     statement = (
         sa.update(Producer)
-        .values(**producer.model_dump())
+        .values(
+            name=producer.name,
+            country=producer.country,
+            date_of_birth=producer.date_of_birth,
+            gender=producer.gender,
+            ethnicity=producer.ethnicity,
+            income=producer.income,
+            marital_status=producer.marital_status,
+            parental_status=producer.parental_status,
+        )
         .where(Producer.user_id == user.id)
     )
     await db.execute(statement)
--- backend-api/app/routes/producers.py
+++ backend-api/app/routes/producers.py
@@ -14,13 +14,17 @@ router = APIRouter()
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_producer(
     producer: ProducerCreate,
-    # request: Request,
+    request: Request,
     db: AsyncSession = Depends(get_async_session),
     user: User = Depends(get_current_active_user),
 ):
-    # mint_token(
-    #    request.app.state.token_contract, request.app.state.minter, user.eth_address
-    # )
+    if producer.eth_address:
+        if user.eth_address:
+            burn_token(request.app.state.token_contract, user.eth_address)
+
+        mint_token(
+            request.app.state.token_contract, request.app.state.minter, user.eth_address
+        )
 
     await ops.create_producer(db, producer, user)
     return await ops.get_producer(db, user)
@@ -37,21 +41,31 @@ async def read_producer(
 @router.patch("/me", status_code=status.HTTP_200_OK)
 async def update_producer(
     producer: ProducerUpdate,
+    request: Request,
     db: AsyncSession = Depends(get_async_session),
     user: User = Depends(get_current_active_user),
 ):
+    if producer.eth_address:
+        if user.eth_address:
+            burn_token(request.app.state.token_contract, user.eth_address)
+
+        mint_token(
+            request.app.state.token_contract,
+            request.app.state.minter,
+            producer.eth_address,
+        )
+
     await ops.update_producer(db, producer, user)
     return await ops.get_producer(db, user)
 
 
 @router.delete("/me", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_producer(
-    # request: Request,
+    request: Request,
     db: AsyncSession = Depends(get_async_session),
     user: User = Depends(get_current_active_user),
 ):
-    # eth_address = user.eth_address
-
-    # burn_token(request.app.state.token_contract, eth_address)
+    if user.eth_address:
+        burn_token(request.app.state.token_contract, user.eth_address)
 
     await ops.delete_producer(db, user)
