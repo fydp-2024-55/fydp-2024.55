afe170de0c97a263c5eca1c1d704c738eb8d9e7d
deandrebaker <deandreb54321@gmail.com>
2024-02-08 23:09:07 -0500

    Remove eth address from user schemas

--- backend-api/app/schemas/users.py
+++ backend-api/app/schemas/users.py
@@ -4,12 +4,12 @@ from fastapi_users import schemas
 
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    eth_address: str | None
+    pass
 
 
 class UserCreate(schemas.BaseUserCreate):
-    eth_address: str | None
+    pass
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    eth_address: str | None
+    pass
