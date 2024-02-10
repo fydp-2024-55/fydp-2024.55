40bfba442f907a728b6dd916ac6f2dee27d280d1
Lu Yao <55671000+luyaojchen@users.noreply.github.com>
2024-02-09 23:27:46 -0500

    Listener for do not enable setting (#28)
    Co-authored-by: Deandre Baker <49622234+deandrebaker@users.noreply.github.com>

--- browser-extension/public/background.js
+++ browser-extension/public/background.js
@@ -38,7 +38,7 @@ chrome.tabs.onRemoved.addListener(async (tabId) => {
 });
 
 // Update tab information when the URL changes
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
   if (
     changeInfo.url &&
     changeInfo.url !== "about:blank" &&
@@ -47,3 +47,58 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     openTabs[tabId] = { url: changeInfo.url, openedAt: Date.now() };
   }
 });
+
+let notify = false;
+
+function checkDNTSetting() {
+  chrome.privacy.websites.doNotTrackEnabled.get({}, function (details) {
+    if (notify === true) {
+      return;
+    }
+
+    if (chrome.runtime.lastError) {
+      console.error(
+        "Error retrieving doNotTrackEnabled:",
+        chrome.runtime.lastError
+      );
+      return;
+    }
+
+    if (
+      details.levelOfControl === "controlled_by_this_extension" ||
+      details.levelOfControl === "controllable_by_this_extension"
+    ) {
+      console.log(
+        `Do Not Track setting is ${details.value ? "enabled" : "disabled"}.`
+      );
+
+      if (details.value === true) {
+        notify = false;
+      }
+
+      if (details.value === false && notify === false) {
+        notify = true;
+        chrome.notifications.create("", {
+          type: "basic",
+          iconUrl: "bytebucks48.ico",
+          title: "Do Not Track",
+          message:
+            "Do Not Track is disabled. Please enable it in your Chrome settings for enhanced privacy.",
+          priority: 2,
+        });
+        setTimeout(() => {
+          notify = false;
+        }, 600000); // im giving them 60 seconds to turn it on
+      }
+    } else {
+      console.log(
+        "Do Not Track setting cannot be controlled by this extension."
+      );
+    }
+  });
+}
+
+// check every 10 seconds and onInstallation
+setInterval(checkDNTSetting, 10000);
+
+chrome.runtime.onInstalled.addListener(checkDNTSetting);
