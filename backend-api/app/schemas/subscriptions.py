b5e1c6ed7c0f1fbacd409a707f7fef9d902e8c93
William Park <w22park@uwaterloo.ca>
2024-02-28 23:08:36 -0800

    Remove consumer_eth_address from SubscriptionItem

--- backend-api/app/schemas/subscriptions.py
+++ backend-api/app/schemas/subscriptions.py
@@ -5,7 +5,6 @@ from pydantic import BaseModel
 
 class SubscriptionItem(BaseModel):
     producer_eth_address: str
-    consumer_eth_address: str
     creation_date: date
     expiration_date: date
     active: bool
@@ -26,15 +25,3 @@ class SubscriptionUpdate(BaseModel):
 
 class SubscriptionDelete(BaseModel):
     producer_eth_addresses: list[str]
-
-
-# class UserData(BaseModel):
-#     gender: str
-#     ethnicity: str
-#     date_of_birth: date
-#     city: str
-#     state: str
-#     country: str
-#     income: int
-#     marital_status: str
-#     parental_status: str
